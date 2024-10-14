2a2f64462f421f5c1f14d164ff795d2422ab3ac8
Gino Andre Anyosa <gino.anyosa@ourlimm.com>
2024-10-13 21:55:19 -0500

    feat: Se actualizo las rutas

--- routes/api.php
+++ routes/api.php
@@ -31,15 +31,23 @@ Route::group(
             Route::post('',[RolController::class,'createRol'])->middleware('validateToken'); 
             Route::get('',[RolController::class,'listRol']); 
             Route::post('update',[RolController::class,'update']);
-            Route::post('change',[RolController::class,'changeStatus']);
+            Route::post('change',[RolController::class,'changeStatus'])->middleware('validaToken');
         });
 
-        Route::group(['prefix'=>'personal'],function(){
+        Route::group(
+            [
+                'prefix'=>'personal',
+                'middleware'=>['validateToken']
+            ],function(){
             Route::get('',[PersonalController::class,'listPersonal']);
             Route::post('',[PersonalController::class,'create']);
         });
 
-        Route::group(['prefix'=>'reserva','middleware'=>['validateToken']],function(){
+        Route::group(
+            [
+                'prefix'=>'reserva',
+                'middleware'=>['validateToken']
+            ],function(){
             Route::get('',[ReservasController::class,'listReservas']);
             Route::post('',[ReservasController::class,'create']);
             Route::post('/disponible',[ReservasController::class,'listHorariosDisponibles']);
